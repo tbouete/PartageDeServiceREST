1b8003684b6390c7ac3b1040fc3236477f7dd4f8
Unknown <guilhem31.susa@hotmail.com>
2020-01-20 16:45:07 +0100

    Client begin pabo

---
+++ Client/script.js
@@ -0,0 +1,38 @@
+
+
+function fillWithServices(){
+	// Récupération d'une référence à la table
+	var tab = document.getElementById("tabServices");
+
+	var i = 0;
+  
+  $.ajax({
+			type: "GET",
+		url: "http://localhost:8080/PartageDeService/api/services",
+		dataType: "xml",
+		success: function(xml) {
+			$(xml).find('services').children('services').each(function(){
+		 
+			  // Insère une ligne dans la table à l'indice de ligne 0
+			  var nouvelleLigne = tab.insertRow(i);
+
+			  // Insère une cellule dans la ligne à l'indice 0
+			  var nouvelleCellule = nouvelleLigne.insertCell(0);
+  			  nouvelleCellule.innerHTML+=$(this).find('ID').text();
+			  var nouvelleCellule = nouvelleLigne.insertCell(1);
+  			  nouvelleCellule.innerHTML+=$(this).find('NOM').text();
+			  var nouvelleCellule = nouvelleLigne.insertCell(2);
+  			  nouvelleCellule.innerHTML+=$(this).find('TYPE').text();
+			  var nouvelleCellule = nouvelleLigne.insertCell(3);
+  			  nouvelleCellule.innerHTML+=$(this).find('PRIX').text();
+			  var nouvelleCellule = nouvelleLigne.insertCell(4);
+  			  nouvelleCellule.innerHTML+=$(this).find('UTILISATEUR').text();			  
+				
+				i++
+			});
+		}
+	});
+
+
+  
+};
