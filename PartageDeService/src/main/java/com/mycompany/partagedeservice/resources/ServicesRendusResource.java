43d5e90fc0621a8da46da8f901b2e2921d91c22e
Unknown <guilhem31.susa@hotmail.com>
2020-01-20 14:45:21 +0100

    Post Create Service Rendu

--- PartageDeService/src/main/java/com/mycompany/partagedeservice/resources/ServicesRendusResource.java
+++ PartageDeService/src/main/java/com/mycompany/partagedeservice/resources/ServicesRendusResource.java
@@ -5,14 +5,19 @@
  */
 package com.mycompany.partagedeservice.resources;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import model.ServiceRendus;
 import model.Singleton;
@@ -86,5 +91,18 @@ public class ServicesRendusResource {
         builder.append("</services_rendus>\n");
         
         return builder.toString();
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public String createServiceRendu(String xml) throws ServerErrorException {
+        ServiceRendus serviceRendu = new ServiceRendus(xml);
+        if(serviceRendu.getService() == null || serviceRendu.getClient() == null || serviceRendu.getNbHeuresConsommees() == null){
+            throw new WebApplicationException(Response.Status.NOT_ACCEPTABLE);
+        }
+        
+        Singleton.listServicesRendus.add(serviceRendu);
+        return serviceRendu.toXML();
     }
 }
